8a900a90349e0a8c140e5989297b94693a614719
Julian Bull <julianbullmagic@gmail.com>
2023-08-18 16:47:42 +1000

    ffff'

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode='geventwebsocket')
+socketio = SocketIO(app, async_mode='gevent')
 
 # Serve the static HTML website
 @app.route('/')
